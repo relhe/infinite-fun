93eba0badcdedf5ac8d674c9c0e667634fc8f8be
Renel Lherisson <renellherisson@gmail.com>
2024-11-03 15:09:42 -0500

    RL | config | ui config file

---
+++ nextjs-frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import BlackPawn from '@/components/chess/pieces/BlackPawn'
+import React from 'react'
+export default function Home() {
+    return (
+        <div>
+            <BlackPawn />
+        </div>
+    )
+}
